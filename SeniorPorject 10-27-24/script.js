8b332a752c279a61e6d8390fa5c4cd496b74c63b
Thomas-C-Dang <107583426+Thomas-C-Dang@users.noreply.github.com>
2024-10-27 14:53:37 -0700

    Add files via upload

---
+++ SeniorPorject 10-27-24/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+function answerCorrect() {
+    console.log("Correct!");
+    alert("Correct!");
+}
+
+function answerIncorrect() {
+    console.log("Incorrect!");
+    alert("Incorrect!");
+}
